94d41eae1145fe2ed4506a71f9b4312a739979e7
serafinpl <ser.burberibar@gmail.com>
2022-03-18 16:34:16 +0100

    styling

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -11,7 +11,11 @@ import {
   Typography,
 } from "@mui/material";
 
+
 const Modal = () => {
+
+const TypoStyleOBJ = { textAlign: "left", color: '#000', height: '50px', m:2, mt:3}
+
   return (
     <div className={classes.Backdrop}>
       <div className={classes.Modal}>
@@ -25,24 +29,26 @@ const Modal = () => {
             noValidate
             autoComplete="off"
           >
-            <Box>
-              <Typography sx={{ textAlign: "left" }}>Report Name</Typography>
-              <Typography sx={{ textAlign: "left" }}>Format</Typography>
-              <Typography sx={{ textAlign: "left" }}>E-mail to</Typography>
-              <Typography sx={{ textAlign: "left" }}>Schedule</Typography>
+            <Box sx={{width: '320px'}}>
+              <Typography sx={TypoStyleOBJ}>Report Name</Typography>
+              <Typography sx={TypoStyleOBJ}>Format</Typography>
+              <Typography sx={TypoStyleOBJ}>E-mail to</Typography>
+              <Typography sx={TypoStyleOBJ}>Schedule</Typography>
             </Box>
             <Box>
               <TextField
                 id="name"
                 label="Shareablee Report"
                 variant="outlined"
+                sx={{width:'95%', height: '50px', m:2,  }}
               />
 
-              <FormControl>
+              <FormControl sx={{ height: '50px', m:1, pl:2, color: '#000'}}>
                 <RadioGroup
                   row
                   aria-labelledby="demo-row-radio-buttons-group-label"
                   name="row-radio-buttons-group"
+                  sx={{ height: '50px', textAlign: 'left'}}
                 >
                   <FormControlLabel
                     value="Excel"
@@ -61,7 +67,37 @@ const Modal = () => {
                 id="email"
                 label="client@company.com"
                 variant="outlined"
+                sx={{width:'95%', height: '50px', m:2}}
               />
+              <FormControl sx={{width:'100%', height: '50px', m:1, pl:2, color: '#000'}}>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="noRepeat"
+                    control={<Radio />}
+                    label="No Repeat"
+                  />
+                  <FormControlLabel
+                    value="date"
+                    control={<Radio />}
+                    label="Specific Date"
+                  />
+                  <FormControlLabel
+                    value="daily"
+                    control={<Radio />}
+                    label="Daily"
+                  />
+                  <FormControlLabel
+                    value="weekly"
+                    control={<Radio />}
+                    label="Weekly"
+                  />
+                  
+                </RadioGroup>
+              </FormControl>
 
             </Box>
           </Box>
